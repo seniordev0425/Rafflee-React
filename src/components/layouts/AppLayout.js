1b6b108aaae7cc0c96384f7472601670d87fba2a
SeniorDev0425 <senior.dev0425@outlook.com>
2020-07-16 22:01:13 +0800

    Add AppLayout.js file

---
+++ src/components/layouts/AppLayout.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import JoinHeader from './HeaderLayout/JoinHeader'
+import Header from './HeaderLayout/Header'
+import FooterLink from './footer/FooterLink'
+import Footer from './footer/Footer'
+
+function AppLayout(props) {
+    const { children } = props
+
+    return (
+        <div style={{ fontFamily: "sofiapro" }}>
+            <div className="parent-header-container">
+                <JoinHeader />
+                <Header />
+            </div>
+            {children}
+            <FooterLink />
+            <Footer />
+        </div>
+    )
+}
+
+export default AppLayout
